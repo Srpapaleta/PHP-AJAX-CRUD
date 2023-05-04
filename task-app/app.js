7c88ff423e8e6542a1fbc30ef91f51ebe5933a5c
Srpapaleta <samith472@gmail.com>
2023-05-04 17:42:36 -0500

    starting app in ajax

---
+++ task-app/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    console.log('jQuery is working');
+
+    $('#search').keyup(function(e){
+        let search = $('#search').val();
+        $.ajax({
+            url: 'task-search.php',
+            type: 'POST',
+            data: {search},
+            success: function(response){
+                let tasks = JSON.parse(response);
+                let template = '';
+                tasks.forEach(task => {
+                    template += `<li>
+                        ${task.name}
+                    </li>`
+                });
+
+                $('#container').html(template);
+            }
+        })
+    })
+});
